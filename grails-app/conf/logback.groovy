878e804ebf02284f1da7bf1bb8823884fa462cc9
Ortiz Galaviz <omar.ortiz.galaviz@accenture.com>
2016-08-09 10:23:59 -0500

    Adding configuation for prodution environment

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,10 +1,11 @@
 import grails.util.BuildSettings
 import grails.util.Environment
 
+def bySecond = timestamp("yyyyMMdd'T'HHmmss")
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%level %logger - %msg%n"
+        pattern = "$bySecond - %level %logger - %msg%n"
     }
 }
 
@@ -16,7 +17,7 @@ if (Environment.isDevelopmentMode() && targetDir) {
         file = "${targetDir}/stacktrace.log"
         append = true
         encoder(PatternLayoutEncoder) {
-            pattern = "%level %logger - %msg%n"
+            pattern = "$bySecond - %level %logger - %msg%n"
         }
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
