d159cfe21b87b9b452e13f84ef30f7d5a8f1a871
Ortiz Galaviz <omar.ortiz.galaviz@accenture.com>
2016-08-08 21:19:10 -0500

    Solve issue when uploading multiple files

--- grails-app/services/com/actions/prototype/comprobantes/ComprobantesService.groovy
+++ grails-app/services/com/actions/prototype/comprobantes/ComprobantesService.groovy
@@ -2,25 +2,28 @@ package com.actions.prototype.comprobantes
 
 import com.actions.prototype.generated.jaxb.comprobantes.Comprobante
 import grails.transaction.Transactional
-import java.text.SimpleDateFormat
 import org.springframework.web.multipart.MultipartFile
 import org.xml.sax.InputSource
 
 import javax.xml.bind.JAXBContext
-import javax.xml.bind.Unmarshaller
+import java.text.SimpleDateFormat
 
 @Transactional
 class ComprobantesService {
 
-    static final Unmarshaller UNMARSHALLER = JAXBContext.newInstance(Comprobante.class).createUnmarshaller()
+    static final JAXBContext JAXB_CONTEXT_INSTANCE = JAXBContext.newInstance(Comprobante.class)
     static final SimpleDateFormat FORMATTER = new SimpleDateFormat('MMMM-yyyy', new Locale('es', 'MX'))
 
     def springSecurityService
 
     def getData() {
-        def types = [Egresos: [], Ingresos: []]
+        def types = [:]
         Document.findAllByUsername(springSecurityService.currentUser.username).each { doc ->
-            types.get(doc.type) << UNMARSHALLER.unmarshal(new InputSource(new ByteArrayInputStream(doc.data)))
+            if(types.get(doc.type) == null) {
+                types.put(doc.type, []);
+            }
+            types.get(doc.type) << JAXB_CONTEXT_INSTANCE.createUnmarshaller().unmarshal(
+                    new InputSource(new ByteArrayInputStream(doc.data)))
         }
 
         def result = [:]
@@ -43,7 +46,8 @@ class ComprobantesService {
     }
 
     def save(MultipartFile file, String type) {
-        Comprobante comprobante = UNMARSHALLER.unmarshal(new InputSource(new ByteArrayInputStream(file.getBytes())))
+        Comprobante comprobante = JAXB_CONTEXT_INSTANCE.createUnmarshaller().unmarshal(
+                new InputSource(new ByteArrayInputStream(file.getBytes())))
         new Document(uuid: comprobante.complemento.timbreFiscalDigital.uuid, name: file.getOriginalFilename(),
                 username: springSecurityService.currentUser.username, data: file.getBytes(), type: type).save()
         return [comprobante: 'Success']
